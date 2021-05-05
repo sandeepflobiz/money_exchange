7fd525790d486c8bef5c0e97f447303cd10efb5f
sandeep <sandeeprajsaimon999@gmail.com>
2021-05-05 21:16:36 +0530

    routes for mini_statement

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'login' => 'authentication#authenticate_user'
   resources :users, only: [:create,:edit,:index,:update]
-  resources :accounts, only: [:create]
+  resources :accounts, only: [:create,:index]
   resources :transfers, only: [:create]
   resources :exchanges, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
