5165217e3c862b718103cccccf7f15d8489382c3
Sandeep Raj Saimon <sandeep@flobiz.in>
2022-03-11 19:15:02 +0530

    updated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :exchanges
   validates :name, presence: true
   # VALID_MOBILE_REGEX = '((\+*)((0[ -]+)*|(91 )*)(\d{12}+|\d{10}+))|\d{5}([- ]*)\d{6}'
-  validates :mobile, presence: true, length: {minimum: 10,maximum: 10}, uniqueness: true
+  validates :mobile, presence: true, length: {minimum: 10,maximum: 10}, uniqueness: true, numericality: true
   validates :password, presence: true, length: {minimum:8 , maximum: 16}
   has_secure_password
 end
